ee6002162a3b1fbf78d5b74a8a6404b983e2b022
nihalpandey4 <61798365+nihalpandey4@users.noreply.github.com>
2020-06-13 18:48:13 +0530

    linked to redis

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const redis = require("redis");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+let client = redis.createClient();
+client.on("connect", function() {
+  console.log("redis connected");
+});
+
+client.flushall();
+
 app.use(express.static("public"));
 const io = require("socket.io")(8080);
-let count = 0;
-let activeCount = 0;
 let users = [];
 
+client.set("HandsRaised",0);
 
 io.on("connection", socket => {
   socket.on("raise-hand", function() {
-    count = count + 1;
-    console.log("Raise Hands " + (count/activeCount)*100+"%");
+    client.incr("HandsRaised");
+    status();
   });
 
   socket.on("hand-raised", function() {
-    count = count - 1;
-    console.log("Raise Hands " + (count/activeCount)*100+"%");
+    client.decr("HandsRaised");
+    status();
   });
 
 
@@ -35,24 +42,31 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+});
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home.ejs");
-  const id= req.sessionID;
+  const id = req.sessionID;
   if (users.indexOf(id) == -1) {
-      users.push(id);
-      activeCount = activeCount + 1;
-      console.log("new user");
-    } else {
-      console.log("somebody returned");
-    }
-    console.log("Active users = "+activeCount);
+    users.unshift(id);
+    client.lpush(["UsersId",id]);
+    console.log("new user");
+  } else {
+    console.log("somebody returned");
+  }
+  status();
 });
 
 app.get("/chatroom",function(req,res){
  res.render("chatroom.ejs");
 });
 
-app.listen(3000,function(){
-  console.log("Server started at port 3000");
-});
+function status() {
+  client.get("HandsRaised",function(err,hands){
+    client.lrange("UsersId",0,-1,function(err,usersList){
+      console.log("Active Users = "+ usersList.length+ "  Hands Raised = "+ (hands/usersList.length)*100+"%");
+    });
+  });
+};
