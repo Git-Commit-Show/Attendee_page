2fab0507722a85e1622999c88e20f3bcacc18464
nihalpandey4 <61798365+nihalpandey4@users.noreply.github.com>
2020-06-13 18:19:37 +0530

    session id now being used

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Attendee_page
-Node js application
+One page application using node.js currently showing percentage of raised hands. Also using session id to keep track of active users
---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+const io = require("socket.io")(8080);
+let count = 0;
+let activeCount = 0;
+let users = [];
+
+
+io.on("connection", socket => {
+  socket.on("raise-hand", function() {
+    count = count + 1;
+    console.log("Raise Hands " + (count/activeCount)*100+"%");
+  });
+
+  socket.on("hand-raised", function() {
+    count = count - 1;
+    console.log("Raise Hands " + (count/activeCount)*100+"%");
+  });
+
+
+});
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+app.use(cookieParser());
+app.use(session({
+    secret: '34SDgsdgspxxxxxxxdfsG', // just a long random string
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+});
+
+app.get("/",function(req,res){
+  res.render("home.ejs");
+  const id= req.sessionID;
+  if (users.indexOf(id) == -1) {
+      users.push(id);
+      activeCount = activeCount + 1;
+      console.log("new user");
+    } else {
+      console.log("somebody returned");
+    }
+    console.log("Active users = "+activeCount);
+});
